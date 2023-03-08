33314c41619210b73ee73c38f2781d5a90c37c2c
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-03-08 19:40:28 +0400

    Rename lesson1.py file and 1.py

--- lesson1.py
+++
@@ -1,5 +0,0 @@
-print("Hello")
-print("What is a programming language?")
-print("What is python and what is the ideology behind the post?Course structure and end goal"
-      "Construction of working environment, python and vscode setup")
-
