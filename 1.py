e8ebbe92fae1dcbc5ba7855116ac7efddfc6b2ae
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-03-08 19:33:56 +0400

    Rename lesson1.py fil and 1.py

---
+++ 1.py
@@ -0,0 +1,5 @@
+print("Hello")
+print("What is a programming language?")
+print("What is python and what is the ideology behind the post?Course structure and end goal"
+      "Construction of working environment, python and vscode setup")
+
