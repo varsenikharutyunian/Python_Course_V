56bfe98722a519be1bd0b9618f66ee2d5d030d46
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-03-16 22:54:33 +0400

    added

--- extension.py
+++ extension.py
@@ -15,19 +15,12 @@ n=45
 print (n//10==n%10)
 n=-44
 print (n//10==n%10)
-"""Create a function that accepts a measurement value in inches 
-and returns the equivalent of the measurement value in feet."""
-
-
-
 
 """3.Write a function that takes an integer minutes and converts it to seconds."""
 a= 5
 print (a*60)
 a=2
 print (a*60)
-
-
 
 """4.Create a function that takes the age in years and returns the age in days."""
 
@@ -38,9 +31,6 @@ print (a*365)
 a=20
 print (a*365)
 
-
-
-
 """5.Write a function that takes two integers (hours, minutes), 
 converts them to seconds, and adds them"""
 def sum(x, y):
@@ -49,7 +39,7 @@ def sum(x, y):
 print(sum(1, 3))
 print(sum(2, 0))
 
-"""Create a function that accepts a measurement value 
+"""6.Create a function that accepts a measurement value 
 in inches and returns the equivalent of the measurement value in feet"""
 
 
@@ -60,7 +50,7 @@ print("The length in Feet",round(Feet,2))
 
 
 
-"""Create a function that takes a number as an argument and returns 
+"""7.Create a function that takes a number as an argument and returns 
 "even" for even numbers and "odd" for odd numbers."""
 
 num = int(input("Enter a number: "))
