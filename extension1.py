71be17652c71cd0f51da9c8b7af241548c090259
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-03-21 08:06:23 +0400

    Added new file

---
+++ extension1.py
@@ -0,0 +1,65 @@
+
+# """1.Emmy has written a function that returns a greeting to users. However, 
+# she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
+# Can you help her"""
+# name = input("username > ")
+
+
+# print("Hello!, " + ((name * (name !="Mubashir" )) or "my love"))
+
+
+
+# my_name = input("enter name  > ")
+# name = my_name
+# "Mubashir"== True
+# result=  "Hello, my Love!"  *  True + "Hello," + name +"!" * (not True)
+# print(result)
+
+    
+
+
+# """2.Create a function that takes two arguments. 
+# Both arguments are integers, a and b. Return True if one of them is 10 or if their sum is 10."""
+# a = 1
+# b = 5
+# c= ( a == 10 or a+b == 10 ) 
+# print(c)
+
+# input_nume = input("enter a,b > ")
+# a=input_nume
+# b=input_nume
+# result =( a == 10 or a+b == 10 ) 
+# print(result)
+
+# """3.Create a function that returns True if an integer is 
+# evenly divisible by 5, and False otherwise."""
+# a=105
+# result = (a % 5) == 0 
+# print (result)
+
+
+# input_nume = input("enter a > ")
+# a = int(input_nume)
+# result = a % 5 == 0
+# # print(result)
+
+# """4.Extra Knowledge 
+# Create a function that takes two strings as arguments and return either True or False depending on whether the total number of characters in the first string is 
+# equal to the total number of characters in the second string."""
+# a ="ADC"
+# b = "CD"
+# result= len(a)==len(b)
+# print(result)
+
+# """5.Given a string, return True if its length is even or False if the length is odd."""
+
+# """C6.reate a function that takes a string txt and a number n and returns the repeated string n number of times.
+# If given argument txt is not a string, return Not A String !!"""
+
+str = input("enter my name > ")
+my_name = str 
+
+result =(my_name * 3),  + (my_name !=("Mubashir")) *"Not A String !!"
+print(result) 
+
+
