dafd55d7c5c705929f003c8b9c419a7a2819e49c
varsenikharutyunian <126870987+varsenikharutyunian@users.noreply.github.com>
2023-03-21 20:22:22 +0400

    Update extension2.py

--- extension2.py
+++ extension2.py
@@ -1,8 +1,1 @@
-# Emmy has written a function that returns a greeting to users.
-# # However, she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
-# name =input("enter name >")
-# y = ("HELLO")  + name
-# x= ("Hello my love")
-# print(x*(name=="Mubashir") + y* ( name != "Mubashir"))
-nam = "Varsenik Harutyunyan"
-print(nam.count(2))
+
