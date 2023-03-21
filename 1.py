4617fba3b399fbe6b58c52775bb293de0522cffb
varsenikharutyunian <varsikharutyunyan72@gmail.com>
2023-03-21 20:02:07 +0400

    rename 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-print("Hello")
-print("What is a programming language?")
-print("What is python and what is the ideology behind the post?Course structure and end goal"
-      "Construction of working environment, python and vscode setup")
-
-x=1000.000000000000000002
-y=1000.0000000000000000056
-print (x==y)
